44dcf01d951b910cfd0bcabb735b1a089ca5740a
GiDebs <giulio.deba@gmail.com>
2022-08-04 16:24:23 +0200

    Update importing.py

--- importing.py
+++ importing.py
@@ -1,3 +1,12 @@
 #This code will import the .csv file downloaded from training peaks
 
 #STEP 1 - create the GUI (Graphical User Interface) in order to allow the user to interact with the code
+
+# Create a window object
+from tkinter import *
+
+app = Tk()
+app.title('Import Data')
+app.geometry('700x350')
+# Start Program
+app.mainloop()
