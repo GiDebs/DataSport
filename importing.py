5c5fd93ae6f540b5b5ae9b59c5fc514ff29e6573
GiDebs <giulio.deba@gmail.com>
2022-08-07 10:17:24 +0200

    testing

--- importing.py
+++ importing.py
@@ -1,14 +1,28 @@
-#This code will import the .csv file downloaded from training peaks
 
-#STEP 1 - create the GUI (Graphical User Interface) in order to allow the user to interact with the code
+from dash import Dash,html
+import pandas as pd
 
-# Create a window object
-from tkinter import * # Import everything from the library tkinter
-from pandas import*
-from plotly import*
+newData = open(r"C:\Users\Giulio\Documents\GitHub\DataSport\Origini\30_05_22-03_07_22.csv")
 
-app = Tk()
-app.title('Import Data')
-app.geometry('700x350')
-# Start Program
-app.mainloop()
+df = pd.read_csv("newData")
+
+def generate_table(dataframe , max_rows=10):
+    return html.Table([html.Thead(
+            html.Tr([html.Th(col) for col in dataframe.columns])
+        ),
+        html.Tbody([
+            html.Tr([
+                html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+            ]) for i in range(min(len(dataframe), max_rows))
+        ])
+    ])
+
+app = Dash(__name__)
+
+app.layout = html.Div([
+    html.H4(children='Workouts'),
+    generate_table(df)
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
